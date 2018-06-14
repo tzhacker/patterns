fb0170f933c6efebf99ac4d3695bea89d05aa8a2
John Tan <zhaohui.tan@dianrong.com>
2018-06-14 18:06:12 +0800

    '小改动'

--- multition/src/main/java/com/yrml/multition/Caller.java
+++ multition/src/main/java/com/yrml/multition/Caller.java
@@ -8,14 +8,15 @@ public class Caller {
   private static final int CALLER_NUMBER = 8;
 
   private static void randomCaller(){
+    System.out.println("------------random---------------");
     for (int i = 0; i < CALLER_NUMBER; i++) {
       MultitionPattern multitionPattern = MultitionPattern.getInstance();
-      System.out.print("第" + (i+1) + "个调用：");
-      multitionPattern.multitionInfo();
+      System.out.println("第" + (i+1) + "个调用：" + multitionPattern.multitionInfo());
     }
   }
 
   private static void pointCaller(){
+    System.out.println("------------point---------------");
     for (int i = 0; i < CALLER_NUMBER; i++) {
       MultitionPattern multitionPattern = null;
       if(i < 3){
@@ -25,8 +26,7 @@ public class Caller {
       } else {
         multitionPattern = MultitionPattern.getInstance(2);
       }
-      System.out.print("第" + (i+1) + "个调用：");
-      multitionPattern.multitionInfo();
+      System.out.println("第" + (i+1) + "个调用：" + multitionPattern.multitionInfo());
     }
   }
 
--- multition/src/main/java/com/yrml/multition/MultitionPattern.java
+++ multition/src/main/java/com/yrml/multition/MultitionPattern.java
@@ -10,11 +10,18 @@ import java.util.Random;
  */
 public class MultitionPattern {
 
+  /**
+   * 最多实例化个数
+   */
   private static final int MAX_NUMBER_OF_MUTITION = 3;
   private static List<String> multitionInfoList = Lists.newArrayListWithExpectedSize(MAX_NUMBER_OF_MUTITION);
   private static List<MultitionPattern> multitionList = Lists.newArrayListWithExpectedSize(MAX_NUMBER_OF_MUTITION);
+  /**
+   * 当前使用的实例化对象
+   */
   private static int countNumberOfMultition = 0;
 
+  //首先把所有的对象都实例化
   static {
     for(int i = 0; i < MAX_NUMBER_OF_MUTITION; i++){
       multitionList.add(new MultitionPattern("Multition" + (i+1) + "号"));
@@ -28,19 +35,32 @@ public class MultitionPattern {
     multitionInfoList.add(info);
   }
 
+  /**
+   * 随机的获取一个实例化对象
+   * @return 实例化对象
+   */
   public static MultitionPattern getInstance(){
     Random random = new Random();
     countNumberOfMultition = random.nextInt(MAX_NUMBER_OF_MUTITION);
     return multitionList.get(countNumberOfMultition);
   }
 
+  /**
+   * 通过对象标识获取指定的实例化对象
+   * @param multition 对象标识
+   * @return 实例化对象
+   */
   public static MultitionPattern getInstance(int multition){
     countNumberOfMultition = multition;
     return multitionList.get(countNumberOfMultition);
   }
 
-  public void multitionInfo(){
-    System.out.println(multitionInfoList.get(countNumberOfMultition));
+  /**
+   * 该实例化对象相关的信息
+   * @return information
+   */
+  public String multitionInfo(){
+    return multitionInfoList.get(countNumberOfMultition);
   }
 
 }
